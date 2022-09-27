8848c8d48a2a750a77191716f56d732ba8824358
Natalia Panfilenka <choco-cat@mail.ru>
2022-09-27 13:06:59 +0300

    feat: solve task

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -4,21 +4,44 @@ use Bitrix\Main\EventManager;
 use Bitrix\Sale;
 
 $eventManager = EventManager::getInstance();
-$eventManager->addEventHandler('sale', 'OnSaleComponentOrderCreated', array('СorrectDiscount', 'OnSaleOrderBeforeSavedHandler'));
+$eventManager->addEventHandler(
+    'sale',
+    'OnSaleComponentOrderCreated',
+    array('CustomBehavior', 'OnSaleOrderBeforeSavedHandler')
+);
 
+$eventManager->addEventHandler(
+    "main",
+    "OnEpilog",
+    array('CustomBehavior', 'OnEpilogHandler')
+);
 
-class СorrectDiscount
+class CustomBehavior
 {
+    public static function OnEpilogHandler()
+    {
+        global $APPLICATION;
+        if (defined("ERROR_404") && ERROR_404 === "Y") {
+            CEventLog::Add(
+                array(
+                    "SEVERITY" => "INFO",
+                    "AUDIT_TYPE_ID" => "ERROR_404",
+                    "MODULE_ID" => "main",
+                    "DESCRIPTION" => $APPLICATION->GetCurUri(),
+                )
+            );
+        }
+    }
 
-public static function OnSaleOrderBeforeSavedHandler(&$order)
-{
-//Получить корзину пользователя
+    public static function OnSaleOrderBeforeSavedHandler(&$order)
+    {
+        //Получить корзину пользователя
         //$basket = Sale\Basket::loadItemsForFUser(Sale\Fuser::getId(), Bitrix\Main\Context::getCurrent()->getSite());
         $basket = $order->getBasket();
-		$abortDiscount = false;
-		
+        $abortDiscount = false;
+
         //Проверка принадлежности товаров к одному разделу
-		foreach ($basket as $basketItem) {
+        foreach ($basket as $basketItem) {
             $els = CIBlockElement::GetList(array(), array("ID" => $basketItem->GetProductID()), false, false, array("PROPERTY_CML2_LINK.IBLOCK_SECTION_ID"));    // получаем товары из корзины
             if ($el = $els->GetNext()) {
                 if (isset($lastSectionId) && $lastSectionId !== $el['PROPERTY_CML2_LINK_IBLOCK_SECTION_ID']) {
@@ -28,25 +51,25 @@ public static function OnSaleOrderBeforeSavedHandler(&$order)
                 }
                 $lastSectionId = $el['PROPERTY_CML2_LINK_IBLOCK_SECTION_ID'];
             }
-			//$basketItem->setField('DISCOUNT_PRICE', 0);
-			//$basketItem->setField('PRICE',  $basketItem->getField('BASE_PRICE'));
+            //$basketItem->setField('DISCOUNT_PRICE', 0);
+            //$basketItem->setField('PRICE',  $basketItem->getField('BASE_PRICE'));
         }
-	if ($abortDiscount) {	
-   $discounts = \Bitrix\Sale\Discount::buildFromOrder($order);
-   $currentDiscountApplied = $discounts->getApplyResult(true)["DISCOUNT_LIST"];
-   foreach(array_keys($currentDiscountApplied) as $key) {
-	$discountsAbort[$key] = 'N';
-   }
+        if ($abortDiscount) {
+            $discounts = \Bitrix\Sale\Discount::buildFromOrder($order);
+            $currentDiscountApplied = $discounts->getApplyResult(true)["DISCOUNT_LIST"];
+            foreach (array_keys($currentDiscountApplied) as $key) {
+                $discountsAbort[$key] = 'N';
+            }
 
-      $discounts->setApplyResult(array('DISCOUNT_LIST' => $discountsAbort)); //отменяем все скидки
-      $discountsCalculateResult = $discounts->calculate();
-      if ($discountsCalculateResult->isSuccess()) {
-         $result = $discountsCalculateResult->getData();
-        $order->applyDiscount($result);
-		//не обновляет корзину??
-		$basket->applyDiscount($result);
-		$basket->save();
-      }
-	}
-}
+            $discounts->setApplyResult(array('DISCOUNT_LIST' => $discountsAbort)); //отменяем все скидки
+            $discountsCalculateResult = $discounts->calculate();
+            if ($discountsCalculateResult->isSuccess()) {
+                $result = $discountsCalculateResult->getData();
+                $order->applyDiscount($result);
+                //не обновляет корзину??
+                $basket->applyDiscount($result);
+                $basket->save();
+            }
+        }
+    }
 }
