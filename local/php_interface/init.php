5b2ceaa4473eb851b9b7a22c2aa66a77a95c978a
Natalia Panfilenka <choco-cat@mail.ru>
2022-09-27 11:40:28 +0300

    feat: add handler to iblock element update

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -4,21 +4,48 @@ use Bitrix\Main\EventManager;
 use Bitrix\Sale;
 
 $eventManager = EventManager::getInstance();
-$eventManager->addEventHandler('sale', 'OnSaleComponentOrderCreated', array('СorrectDiscount', 'OnSaleOrderBeforeSavedHandler'));
+$eventManager->addEventHandler(
+    'sale',
+    'OnSaleComponentOrderCreated',
+    array('customizeBehavior', 'OnSaleOrderBeforeSavedHandler')
+);
+$eventManager->addEventHandler(
+    'iblock',
+    'OnBeforeIBlockElementUpdate',
+    array('customizeBehavior', 'OnDeactivationCheck')
+);
 
-
-class СorrectDiscount
+class customizeBehavior
 {
+    const PRODUCT_IBLOCK_ID = 2;
+    const SHOW_COUNT = 2;
 
-public static function OnSaleOrderBeforeSavedHandler(&$order)
-{
-//Получить корзину пользователя
+    //Запретить деактивировать товар, если просмотров больше SHOW_COUNT
+    public static function OnDeactivationCheck(&$arFields)
+    {
+        global $APPLICATION;
+        if ($arFields['ACTIVE'] !== 'Y' && $arFields['IBLOCK_ID'] === static::PRODUCT_IBLOCK_ID) {
+            $res = CIBlockElement::GetList(array(), array("ID" => $arFields["ID"]), false, false, array("SHOW_COUNTER"));
+            if ($arRes = $res->GetNext()) {
+                if ($arRes['SHOW_COUNTER'] > static::SHOW_COUNT) {
+                    $APPLICATION->throwException('Товар невозможно деактировать, у него ' .
+                        $arRes['SHOW_COUNTER'] . ' просмотров');
+                    return false;
+                }
+            }
+        }
+    }
+
+    //Отменить скидки в заказе, если товары из разных разделов
+    public static function OnSaleOrderBeforeSavedHandler(&$order)
+    {
+        //Получить корзину пользователя
         //$basket = Sale\Basket::loadItemsForFUser(Sale\Fuser::getId(), Bitrix\Main\Context::getCurrent()->getSite());
         $basket = $order->getBasket();
-		$abortDiscount = false;
-		
+        $abortDiscount = false;
+
         //Проверка принадлежности товаров к одному разделу
-		foreach ($basket as $basketItem) {
+        foreach ($basket as $basketItem) {
             $els = CIBlockElement::GetList(array(), array("ID" => $basketItem->GetProductID()), false, false, array("PROPERTY_CML2_LINK.IBLOCK_SECTION_ID"));    // получаем товары из корзины
             if ($el = $els->GetNext()) {
                 if (isset($lastSectionId) && $lastSectionId !== $el['PROPERTY_CML2_LINK_IBLOCK_SECTION_ID']) {
@@ -28,25 +55,25 @@ public static function OnSaleOrderBeforeSavedHandler(&$order)
                 }
                 $lastSectionId = $el['PROPERTY_CML2_LINK_IBLOCK_SECTION_ID'];
             }
-			//$basketItem->setField('DISCOUNT_PRICE', 0);
-			//$basketItem->setField('PRICE',  $basketItem->getField('BASE_PRICE'));
+            //$basketItem->setField('DISCOUNT_PRICE', 0);
+            //$basketItem->setField('PRICE',  $basketItem->getField('BASE_PRICE'));
         }
-	if ($abortDiscount) {	
-   $discounts = \Bitrix\Sale\Discount::buildFromOrder($order);
-   $currentDiscountApplied = $discounts->getApplyResult(true)["DISCOUNT_LIST"];
-   foreach(array_keys($currentDiscountApplied) as $key) {
-	$discountsAbort[$key] = 'N';
-   }
-
-      $discounts->setApplyResult(array('DISCOUNT_LIST' => $discountsAbort)); //отменяем все скидки
-      $discountsCalculateResult = $discounts->calculate();
-      if ($discountsCalculateResult->isSuccess()) {
-         $result = $discountsCalculateResult->getData();
-        $order->applyDiscount($result);
-		//не обновляет корзину??
-		$basket->applyDiscount($result);
-		$basket->save();
-      }
-	}
-}
+        if ($abortDiscount) {
+            $discounts = \Bitrix\Sale\Discount::buildFromOrder($order);
+            $currentDiscountApplied = $discounts->getApplyResult(true)["DISCOUNT_LIST"];
+            foreach (array_keys($currentDiscountApplied) as $key) {
+                $discountsAbort[$key] = 'N';
+            }
+            //отменяем все скидки
+            $discounts->setApplyResult(array('DISCOUNT_LIST' => $discountsAbort));
+            $discountsCalculateResult = $discounts->calculate();
+            if ($discountsCalculateResult->isSuccess()) {
+                $result = $discountsCalculateResult->getData();
+                $order->applyDiscount($result);
+                //не обновляет корзину??
+                $basket->applyDiscount($result);
+                $basket->save();
+            }
+        }
+    }
 }
